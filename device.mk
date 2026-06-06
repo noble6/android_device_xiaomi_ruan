477fbfe2b3b92daeecf870984e7a3a36a7dced21
noble6 <techin187@gmail.com>
2026-06-06 06:14:57 +0000

    ruan: Add fstab.default for stock kernel first stage init compatibility

--- device.mk
+++ device.mk
@@ -192,7 +192,8 @@ PRODUCT_PACKAGES += \
     ueventd.qcom.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.default
 
 # IPACM
 PRODUCT_PACKAGES += \
